169b07dd9b81fc55557e8d7a8caee720c7f4054c
Manraj Singh <manrajsinghgrover@gmail.com>
2015-10-03 01:10:08 +0530

    Reduced number of clicks

--- hack-kuku-kube.js
+++ hack-kuku-kube.js
@@ -1,9 +1,9 @@
 var findIt = function(){
     var boxes = document.getElementById("box").getElementsByTagName("span");
+    boxes[0].click();
     for(var i=1;i<boxes.length;i++){
         if(boxes[i].style.backgroundColor!=boxes[i-1].style.backgroundColor){
             boxes[i].click();
-            boxes[i-1].click();
             break;
         }
     }
