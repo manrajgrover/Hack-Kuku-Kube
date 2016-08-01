377b9ff765b40a3349c5b0ccd695651115dcba44
Manraj Singh <manrajsinghgrover@gmail.com>
2016-08-01 19:09:05 +0530

    Indentation fixed

--- hack-kuku-kube.js
+++ hack-kuku-kube.js
@@ -1,12 +1,12 @@
 var findIt = function(){
-    var boxes = document.getElementById("box").getElementsByTagName("span");
-    boxes[0].click();
-    for(var i=1;i<boxes.length;i++){
-        if(boxes[i].style.backgroundColor!=boxes[i-1].style.backgroundColor){
-            boxes[i].click();
-            break;
-        }
+  var boxes = document.getElementById("box").getElementsByTagName("span");
+  boxes[0].click();
+  for(var i=1;i<boxes.length;i++){
+    if(boxes[i].style.backgroundColor!=boxes[i-1].style.backgroundColor){
+      boxes[i].click();
+      break;
     }
+  }
 }
 var hackRate = 1; // In Milliseconds
 setInterval(function(){findIt();},hackRate);
